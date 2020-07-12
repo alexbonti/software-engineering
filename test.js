85d151ec65918618bc456a3234c1c9a5b5e39c7e
alessio bonti <alexbonti83@hotmail.com>
2020-07-12 10:22:17 +1000

    created the biosensor and tested

--- test.js
+++ test.js
@@ -1,2 +1,25 @@
-console.log('Hello world')
-// i changed this online 
+// crete a biosensor simulator 
+// start with the heart
+
+randomNumber=function(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+}
+class Heart{
+    constructor(age,name){
+        this.age=age
+        this.owner=name
+        this.lastHR=0
+        this.live()
+    }
+
+    beat(){
+        this.lastHR=randomNumber(60,70)
+        console.log('[Owner]:'+this.owner+' - Heart Rate='+this.lastHR)
+    }
+    live(){
+        setInterval(()=>{this.beat()},1000)
+    }
+}
+
+let james=new Heart(28,'james')
+let sara=new Heart(15,'Sara')
